32c0e8d862bfb8b4effbe659f31c827887bbc6e2
icirauqui <icirauqui@gmail.com>
2021-10-17 14:52:27 +0200

    Added environment mountain car continuous

---
+++ environments/env_mountain_car_cont.cpp
@@ -0,0 +1,159 @@
+#include "env_mountain_car_cont.hpp"
+
+env_mountain_car_cont::env_mountain_car_cont(bool bdbg){
+    this->bDebug = bdbg;
+}
+
+env_mountain_car_cont::~env_mountain_car_cont(){}
+
+
+void env_mountain_car_cont::debug_mode(bool dbg){
+    this->bDebug = dbg;
+}
+
+
+std::string env_mountain_car_cont::get_env_id(){
+    return envId;
+}
+
+
+void env_mountain_car_cont::step(float action, Eigen::RowVectorXf &zstate, float &zreward, bool &zdone){
+    x = state.coeffRef(0);
+    v = state.coeffRef(1);
+
+    float force = action_min;
+    if (action > action_min)
+        force = action;
+    if (force > action_max)
+        force = action_max;
+    
+    v += (force * power) - (0.0025 * cos(3*x));
+
+    if (v > speed_max)
+        v = speed_max;
+    if (v < -speed_max)
+        v = -speed_max;
+    
+    x += v;
+
+    if (x > position_max)
+        x = position_max;
+    if (x < position_min)
+        x = position_min;
+
+    if (x == position_min && v < 0)
+        v = 0.0;
+    
+    bool done = false;
+    if (x >= position_goal && v >= velocity_goal)
+        done = true;
+    else 
+        done = false;
+    
+    float reward = 0.0;
+    if (done)
+        reward = 100.0;
+    reward -= 0.1 * pow(action,2);
+
+    state.coeffRef(0) = x;
+    state.coeffRef(1) = v;
+    zstate = state;
+    zreward = reward;
+    zdone = done;
+}
+
+
+Eigen::RowVectorXf* env_mountain_car_cont::reset(){
+    float LO = -0.6;
+    float HI = -0.4;
+    state.coeffRef(0) = LO + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(HI-LO)));
+    state.coeffRef(1) = 0.0;
+
+    return &state;
+}
+
+
+std::vector<float> env_mountain_car_cont::_height(std::vector<float> xs){
+    std::vector<float> ys;
+    for (unsigned int i=0; i<xs.size(); i++){
+        float xs1 = sin(3 * xs[i]) * 0.45 + 0.55;
+        ys.push_back(xs1);
+    }
+    return ys;
+}
+
+
+float env_mountain_car_cont::_height(float xs){
+    float ys = sin(3 * xs) * 0.45 + 0.55;
+    return ys;
+}
+
+
+void env_mountain_car_cont::render(int framerate){
+
+    float screen_width = 600;
+    float screen_height = 400;
+
+    float world_width = position_max - position_min;
+    float scale = screen_width / world_width;
+    float carwidth = 40;
+    float carheight = 20;
+    float wheelrad = carheight/2.5;
+
+
+
+    img = cv::Mat(screen_height,screen_width,CV_8UC3, cv::Scalar(255,255,255));
+
+    std::vector<float> xpoly, ypoly;
+    float pt1 = position_min;
+    xpoly.push_back(pt1);
+    for (unsigned int i=0; i<100; i++){
+        pt1 += (position_max - position_min)/100;
+        xpoly.push_back(pt1);
+    }
+    ypoly = _height(xpoly);
+
+    std::vector<cv::Point> contour;
+    for (unsigned int i=0; i<xpoly.size(); i++)
+        contour.push_back(cv::Point(scale*(xpoly[i]-position_min),screen_height - scale*ypoly[i]));
+
+    const cv::Point *pts = (const cv::Point*) cv::Mat(contour).data;
+    int npts = cv::Mat(contour).rows;
+
+    polylines(img, &pts, &npts, 1, false, cv::Scalar(0, 0, 0), 3);
+
+    float clearance = 10;
+
+    float l = -carwidth / 2;
+    float r = carwidth / 2;
+    float t = carheight;
+    float b = 0.0;
+
+
+    float x = state.coeffRef(0);
+    float y = _height(x);
+    float xpos = scale*(x-position_min);
+    float ypos = screen_height - scale*y;
+    //std::cout << x << " " << vxpos[0] << " " << xpos << std::endl;
+    //std::cout << vypos[0] << " " << ypos << std::endl;
+
+    cv::Point rook_points[1][4];
+    rook_points[0][0]  = cv::Point(xpos+l, ypos+b);
+    rook_points[0][1]  = cv::Point(xpos+l, ypos+t);
+    rook_points[0][2]  = cv::Point(xpos+r, ypos+t);
+    rook_points[0][3]  = cv::Point(xpos+r, ypos+b);
+
+    const cv::Point* ppt[1] = { rook_points[0] };
+    int npt[] = { 4 };
+    fillPoly(img, ppt, npt, 1, cv::Scalar(0,0,0), cv::LINE_8 );
+
+
+
+
+
+
+
+    cv::namedWindow("Render",cv::WINDOW_AUTOSIZE);
+    cv::imshow("Render",img);
+    cv::waitKey(framerate);
+}
---
+++ environments/env_mountain_car_cont.hpp
@@ -0,0 +1,94 @@
+#ifndef ENV_MOUNTAIN_CAR_CONT_HPP
+#define ENV_MOUNTAIN_CAR_CONT_HPP
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <eigen3/Eigen/Eigen>
+
+#define PI 3.14159265358979323846
+
+
+/*
+Description:
+    The agent (a car) starts at the bottom of a valley. For any given state 
+    the agent may choose to accelerate to the left, right, or not accelerate.
+Source:
+    Adaptation of the MountainCar Environment from the "FAReinforcement" library
+    of Jose Antonio Martin H. (version 1.0), adapted by  'Tom Schaul, tom@idsia.ch'
+    and then modified by Arnaud de Broissia
+Observation:
+    Type: Box(4)
+    Num     Observation               Min                     Max
+    0       Car Position              -1.2                    0.6
+    1       Car Velocity              -0.07                   0.07
+Actions:
+    Type: Box(2)
+    Num     Action                    Min                     Max
+    0       the power coef            -1.0                    1.0
+    Note: actual driving force is calculated by multipling the power coef by power (0.0015)
+Reward:
+    Reward of 100 is awarded if the agent reached the flag (position = 0.45) on top of the mountain.
+    Reward is decrease based on amount of energy consumed each step.
+Starting State:
+    The position of the car is assigned a uniform random value in [-0.6 , -0.4].
+    The starting velocity of the car is always assigned to 0.
+Episode Termination:
+     The car position is more than 0.45
+    Episode length is greater than 200
+*/
+
+
+
+
+class env_mountain_car_cont{
+
+    private:
+
+        bool bDebug = false;
+        std::string envId = "MountainCarCont";
+
+        float action_min = -1.0;
+        float action_max = 1.0;
+        float position_min = -1.2;
+        float position_max = 0.6;
+        float speed_max = 0.07;
+        float position_goal = 0.45;
+
+        float power = 0.0015;
+        float velocity_goal = 0.0;
+
+
+        float x = 0.0; // position
+        float v = 0.0; // speed
+        Eigen::RowVectorXf state = Eigen::RowVectorXf(2);
+
+        // Render 
+        cv::Mat img = cv::Mat(600,800,CV_8UC3, cv::Scalar(255,255,255));
+
+    public:
+
+        env_mountain_car_cont(bool bdbg = false);
+        ~env_mountain_car_cont();
+
+        void debug_mode(bool dbg);
+        std::string get_env_id();
+
+        void step(float action, Eigen::RowVectorXf &zstate, float &zreward, bool &zdone);
+
+        Eigen::RowVectorXf* reset();
+
+        std::vector<float> _height(std::vector<float> xs);
+        float _height(float xs);
+        void render(int framerate = 33);
+
+
+};
+
+
+
+#endif
